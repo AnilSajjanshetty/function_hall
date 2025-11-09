474df94e13875e6fe6a7021fa7d5ddb2730c2b3e
AnilSajjanshetty <anilsajjanshetty112@gmail.com>
2025-11-09 22:07:38 +0530

    fix:cores error

--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+// Middleware
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
